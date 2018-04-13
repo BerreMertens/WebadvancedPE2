0a78b93b613d2e9636cee12dfc1adc29997f21de
vies dikke zeikmuil <Berre.Mertens@student.pxl.be>
2018-04-13 16:25:00 +0200

    bugfix dubbel locatie klikken (niet ingelogd)

--- resources/views/menu.blade.php
+++ resources/views/menu.blade.php
@@ -2,13 +2,13 @@
 <nav class="navbar navbar navbar-default">
     <div class="container-fluid">
         <div class="navbar-header">
-            <a class="navbar-brand" href="#">onbekwaam</a>
+            <a class="navbar-brand" href="/home">onbekwaam</a>
         </div>
         <ul class="nav navbar-nav ">
 
             @if(Auth::check())
                 <li ><a href="/home">Home</a></li>
-                <li><a class="navbar-link" href="/locatie/LocatieLijstGebruiker">Locaties</a></li>
+                <li><a  href="/locatie/LocatieLijstGebruiker">Locaties</a></li>
                 <li><a href="/locatie">{{Auth::user()->name}}</a></li>
                 <li><a href="{{url('logout')}}">Log Out</a></li>
 
@@ -16,7 +16,7 @@
 
             @else
                 <li ><a href="/home">Home</a></li>
-                <li><a href="locatie/LocatieLijstGebruiker">Locaties</a></li>
+                <li><a href="/locatie/LocatieLijstGebruiker">Locaties</a></li>
                 <li><a href="/Login">Login</a></li>
             @endif
         </ul>
